cdebb5a72a2dad421b0e62cbf1af926dbcd121cc
Aleksei Khabirov <finko85@gmail.com>
2021-05-18 22:10:50 +0300

    feat: add linter and github actions

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
-
-export const greetUser = () => {
+export default () => {
   console.log('Welcome to the Brain Games!');
 
   const name = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${name}`);
-}
+};
